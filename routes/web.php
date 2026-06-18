cc2367945891b0c5187cfdce123bde00f0002e41
amevafarosh <122461669+amevafarosh@users.noreply.github.com>
2026-06-18 10:02:24 +0530

    fix react catch-all route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/{any?}', function () {
     return response()->file(
         public_path('frontend/index.html')
     );
-})->where('any', '.*');
+})->where('any', '^(?!frontend/).*$');
